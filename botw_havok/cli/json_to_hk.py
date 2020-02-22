7770b956081965d8a7e5c54b44ed4590bebbff3d
Martin Kröner <kronerm9@gmail.com>
2020-02-22 22:06:13 +0100

    Fix json_to_hk command

--- botw_havok/cli/json_to_hk.py
+++ botw_havok/cli/json_to_hk.py
@@ -13,8 +13,14 @@ def main():
     parser.add_argument("hkFile", help="Path to destination Havok packfile")
     args = parser.parse_args()
 
-    hk = Havok.from_file(args.hkFile)
-    hk.to_json(args.jsonFile)
+    hk = Havok.from_json(args.jsonFile)
+
+    if args.nx:
+        hk.to_switch()
+    else:
+        hk.to_wiiu()
+
+    hk.to_file(args.hkFile)
 
 
 if __name__ == "__main__":
