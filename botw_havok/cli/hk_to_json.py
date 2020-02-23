213921241f8c183141ed0791f8d6cda0595eafbc
Martin Kröner <kronerm9@gmail.com>
2020-02-23 17:31:44 +0100

    Added more convenient cli options

--- botw_havok/cli/hk_to_json.py
+++ botw_havok/cli/hk_to_json.py
@@ -5,14 +5,19 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Convert Havok packfile to JSON")
     parser.add_argument("hkFile", help="Path to a Havok packfile")
-    parser.add_argument("jsonFile", help="Path to destination JSON file")
+    parser.add_argument("-o", "--out", help="Path to destination JSON file")
     parser.add_argument(
         "-p", "--pretty-print", action="store_true", help="Pretty-print the JSON"
     )
     args = parser.parse_args()
 
+    if args.out:
+        jsonFile = args.out
+    else:
+        jsonFile = ".".join(args.hkFile.split(".")[:-1]) + ".json"
+
     hk = Havok.from_file(args.hkFile)
-    hk.to_json(args.jsonFile, args.pretty_print)
+    hk.to_json(jsonFile, args.pretty_print)
 
 
 if __name__ == "__main__":
--- botw_havok/cli/json_to_hk.py
+++ botw_havok/cli/json_to_hk.py
@@ -10,17 +10,22 @@ def main():
         "--nx", action="store_true", help="Convert to Nintendo Switch packfile"
     )
     parser.add_argument("jsonFile", help="Path to a JSON file")
-    parser.add_argument("hkFile", help="Path to destination Havok packfile")
+    parser.add_argument("-o", "--out", help="Path to destination Havok packfile")
     args = parser.parse_args()
 
     hk = Havok.from_json(args.jsonFile)
+
+    if args.out:
+        hkFile = args.out
+    else:
+        hkFile = ".".join(args.jsonFile.split(".")[:-1]) + ".hkx"
 
     if args.nx:
         hk.to_switch()
     else:
         hk.to_wiiu()
 
-    hk.to_file(args.hkFile)
+    hk.to_file(hkFile)
 
 
 if __name__ == "__main__":
