b51753c8cd44c5cfb9b394f4012e131adf1c3df1
Martin Kröner <kronerm9@gmail.com>
2020-05-07 09:49:16 +0200

    add pretty-print to hk_to_json command

--- botw_havok/cli/hk_to_json.py
+++ botw_havok/cli/hk_to_json.py
@@ -8,11 +8,14 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Convert Havok packfile to JSON")
     parser.add_argument("hkFile", help="Path to a Havok packfile")
     parser.add_argument("outFile", help="Path to destination JSON file", nargs="?")
+    parser.add_argument(
+        "-p", "--pretty-print", help="Pretty-print the JSON file", action="store_true"
+    )
 
     return parser.parse_args()
 
 
-def hk_to_json(hkFile: str, outFile: str):
+def hk_to_json(hkFile: str, outFile: str, pretty_print: bool):
     if not outFile:
         outFile = change_extension(hkFile, "json")
         check_if_exists(outFile)
@@ -24,7 +27,7 @@ def hk_to_json(hkFile: str, outFile: str):
     hk.deserialize()
 
     Messages.writing(outFile)
-    hk.to_json(outFile, pretty_print=True)
+    hk.to_json(outFile, pretty_print=pretty_print)
 
 
 def main():
@@ -32,7 +35,7 @@ def main():
 
     args = parse_args()
 
-    hk_to_json(args.hkFile, args.outFile)
+    hk_to_json(args.hkFile, args.outFile, args.pretty_print)
 
     Messages.done()
 
